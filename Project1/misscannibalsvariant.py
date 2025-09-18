add21f301dc9a6cf595a2eb8bfb36d6d3d8dbf2f
christian1251 <christianc905@csu.fullerton.edu>
2025-09-18 09:08:58 -0700

    added search.py

--- Project1/misscannibalsvariant.py
+++ Project1/misscannibalsvariant.py
@@ -14,8 +14,13 @@ class MissCannibalsVariant(Problem):
         self.N1 = N1
         self.N2 = N2
         super().__init__(initial, goal)
-
-
+        
+    
+        
+        
+        
+        
+        
 if __name__ == '__main__':
     mc = MissCannibalsVariant(4,4)
     # print(mc.actions((3, 3, True))) # Test your code as you develop! This should return  ['MC', 'MMM']
